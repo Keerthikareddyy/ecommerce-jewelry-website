bb4f1a3c29cab8f4980a82acba8e0718944d122b
Keerthikareddyy <keerthikareddygaddam@gmail.com>
2025-08-11 20:56:17 +0530

    Update views.py

--- store/views.py
+++ store/views.py
@@ -9,7 +9,6 @@ import decimal
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator # for Class Based Views
 
-
 # Create your views here.
 
 def home(request):
